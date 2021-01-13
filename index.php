43a8987f7e9c8a1de3b03842ca454321a4ceec9c
Kai Fergerstrom <kaifergerstrom@gmail.com>
2021-01-12 22:04:39 -0500

    Minimalist start screen

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en" class="uk-height-1-1">
+	<head>
+		<title>Title</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.10/dist/css/uikit.min.css" />
+		<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.10/dist/js/uikit.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/uikit@3.6.10/dist/js/uikit-icons.min.js"></script>
+		<link rel="stylesheet" href="index.css" />
+	</head>
+	<body class="uk-height-1-1">
+		<nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+			<div class="uk-navbar-center">
+				<ul class="uk-navbar-nav">
+					<li class="uk-active"><a href="#">Home</a></li>
+					<li><a href="#">Support</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div>
+			<div class="uk-position-center">
+				<div>
+					<h1 class="uk-text-bold">JobDocs <span class="uk-text-light secondary-color">Mobile</span></h1>
+					<p class="uk-text-meta">Built for Contractor by Contractors.</p>
+				</div>
+				<div class="uk-margin">
+					<input class="uk-input" type="text" placeholder="Enter Work Order ID">
+				</div>
+			</div>
+			<div class="footer uk-position-bottom">
+				<ul class="circles">
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
